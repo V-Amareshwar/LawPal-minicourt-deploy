ea56e4309fa25c6b02a20ca6affe5ee0c0d2acec
V-Amareshwar <179671725+V-Amareshwar@users.noreply.github.com>
2026-05-15 20:45:52 +0530

    Remove hardcoded wildcard CORS header breaking EventSource

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -174,7 +174,6 @@ module.exports = (authenticateToken, checkDatabaseConnection, io, groqAxios) => 
             res.setHeader('Content-Type', 'text/event-stream');
             res.setHeader('Cache-Control', 'no-cache');
             res.setHeader('Connection', 'keep-alive');
-            res.setHeader('Access-Control-Allow-Origin', '*');
             res.flushHeaders();
 
             res.write(`data: ${JSON.stringify({ type: 'thinking', done: false })}\n\n`);
